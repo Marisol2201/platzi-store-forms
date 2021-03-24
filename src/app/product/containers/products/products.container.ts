2555d7009033f4ecdee39a03eace0fbeabb792e3
Marisol2201 <1529@holbertonschool.com>
2021-03-24 10:21:18 -0500

    Patrón smart and dumb aplicado con products

--- src/app/product/containers/products/products.container.ts
+++ src/app/product/containers/products/products.container.ts
@@ -5,10 +5,10 @@ import { ProductsService } from './../../../core/services/products/products.serv
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  templateUrl: './products.container.html',
+  styleUrls: ['./products.container.scss']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsContainer implements OnInit {
 
   products: Product[] = [];
 
